6defe617dcccea5d6ec83dec041beb704ae9fe0f
harshKumar029 <harshanand029@gmail.com>
2024-06-11 12:59:10 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -82,9 +82,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+// https://x-agc4.onrender.com
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://x-three-plum.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://linked-mu.vercel.app");
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
