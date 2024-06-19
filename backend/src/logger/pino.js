9e0ceaae720083d30b7712d77b315c8eec9a9bb8
harshKumar029 <harshanand029@gmail.com>
2024-06-19 23:50:09 +0530

    pino

--- backend/src/logger/pino.js
+++ backend/src/logger/pino.js
@@ -1,16 +1,16 @@
-// const pino = require("pino");
+const pino = require("pino");
 
-// const logger = pino({
-//     transport:{
-//         target: "pino-pretty",
-//         options:{
-//             translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-//             IGNORE:"paid,hostname",
-//         }
-//     }
-//   });
+const logger = pino({
+    transport:{
+        target: "pino-pretty",
+        options:{
+            translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+            IGNORE:"paid,hostname",
+        }
+    }
+  });
 
-//   module.exports = logger;
+  module.exports = logger;
 
 
 // const pino = require("pino");
@@ -23,17 +23,17 @@
 
 // module.exports = logger;
 
-const pino = require("pino");
+// const pino = require("pino");
 
-const logger = pino({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug', // Set log level based on environment
-    transport: process.env.NODE_ENV !== 'production' ? {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
-        }
-    } : undefined
-});
+// const logger = pino({
+//     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+//     transport: process.env.NODE_ENV !== 'production' ? {
+//         target: 'pino-pretty',
+//         options: {
+//             colorize: true
+//         }
+//     } : undefined
+// });
 
-module.exports = logger;
+// module.exports = logger;
 
