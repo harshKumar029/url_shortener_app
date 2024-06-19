6fcc850b1b5c7c1425091fe2f57c0e9c8c794b9a
harshKumar029 <harshanand029@gmail.com>
2024-06-19 21:33:06 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -88,7 +88,7 @@ app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    );+
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
