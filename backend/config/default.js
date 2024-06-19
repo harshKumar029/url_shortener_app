5c3bc7d6b3603ffa247836933f15c3a62545bb11
harshKumar029 <harshanand029@gmail.com>
2024-06-20 00:19:15 +0530

    default .js

--- backend/config/default.js
+++ backend/config/default.js
@@ -13,7 +13,7 @@ module.exports = {
     // host,
     saltWorkFactor,
     secret_key,
-    nodeEnv: process.env.NODE_ENV || 'production'
+    // nodeEnv: process.env.NODE_ENV || 'development'
 };
 
 // require('dotenv').config();
