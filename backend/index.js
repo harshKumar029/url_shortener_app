805c589c4fafb1c286377eaf203d869f4160faa3
harshKumar029 <harshanand029@gmail.com>
2024-06-19 20:25:19 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -83,15 +83,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // https://x-agc4.onrender.com
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     next();
+// });
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+}));
 
 app.get('/:shortURL', async (req, res) => {
     const shortURL = req.params.shortURL;
