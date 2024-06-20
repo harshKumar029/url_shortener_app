bed0d71aa26bbe83438b4d8f8e93c3c2eb05a623
harshKumar029 <harshanand029@gmail.com>
2024-06-20 15:54:36 +0530

    pino

--- backend/src/logger/pino.js
+++ backend/src/logger/pino.js
@@ -1,40 +1,13 @@
-// const pino = require("pino");
-
-// const logger = pino({
-//     transport:{
-//         target: "pino-pretty",
-//         options:{
-//             translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-//             IGNORE:"paid,hostname",
-//         }
-//     }
-//   });
-
-//   module.exports = logger;
-
-
-// const pino = require("pino");
-// const pinoPretty = require("pino-pretty");
-
-// const logger = pino({
-//     prettifier: pinoPretty, // Use pino-pretty for formatting logs in development
-//     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug' // Set log level based on environment
-// });
-
-// module.exports = logger;
-
 const pino = require("pino");
 
 const logger = pino({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    transport: process.env.NODE_ENV !== 'production' ? {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
+    transport:{
+        target: "pino-pretty",
+        options:{
+            translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+            IGNORE:"paid,hostname",
         }
-    } : undefined
-});
+    }
+  });
 
-module.exports = logger;
-
-
+  module.exports = logger;
